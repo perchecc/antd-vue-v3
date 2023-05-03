865acc35115a7dea495444606c42054248d5ae38
edwin <perchecc@163.com>
2023-05-04 01:04:53 +0800

    chore:init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import { RouterView } from "vue-router";
+import { reactive, toRefs } from "vue";
+
+export default {
+  name: "App",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      redirect: "/todo",
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/todo",
+      component: () => import("../views/todo.vue"),
+    },
+  ],
+});
+
+export default router;
